779d2d3aad2ac87ec06eaa52bcc89d94dc7461d4
lrgarrison <lrgarrison@users.noreply.github.com>
2017-12-13 14:05:19 -0600

    Add files via upload

---
+++ Screens/home.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
