6fd19fe3f2628850d1ba87cd852d87f35b974738
ahunter135 <ahunter8850@outlook.com>
2018-01-22 16:15:30 -0600

    syncing up

--- server/server.js
+++ server/server.js
@@ -50,6 +50,16 @@ app.post('/submitForm', function(req, res) {
   })
 })
 
+app.post('/getAllForms', function(req, res) {
+  getAllForms(function(err, result) {
+    if (err) {
+      res.send(err);
+    } else if (result) {
+      res.send(result);
+    }
+  })
+})
+
 /*
 https.createServer(options, app).listen(8005, function() {
   console.log('running on port 8005');
@@ -97,4 +107,22 @@ submitForm(form, callback) {
     });
     db.close();
   });
+}
+
+getAllForms(callback) {
+  MongoClient.connect(url, function(err, db) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("We are connect to get all forms");
+    }
+    var collection = db.collection('Forms');
+    collection.find().toArray(function(err, result) {
+      if (err) {
+        callback(err);
+      } else if (result) {
+        callback(result);
+      }
+    });
+  })
 }
